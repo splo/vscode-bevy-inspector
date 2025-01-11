2bfc3314d6875a07f5181fcd22f841f80df9a81c
Sébastien Job <sebastien.job@gmail.com>
2025-01-11 18:44:19 +0100

    feat: Add Go to Defintion to components
    #Closes #10

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,4 @@
+import levenshtein from 'fast-levenshtein';
 import * as vscode from 'vscode';
 import { BevyTreeDataProvider } from './bevyTreeDataProvider';
 import { BevyTreeService, Component, DEFAULT_BEVY_VERSION, Entity } from './bevyViewService';
@@ -25,6 +26,7 @@ export function activate(context: vscode.ExtensionContext) {
 			await vscode.env.clipboard.writeText(component.name);
 		}
 	});
+	vscode.commands.registerCommand('bevyInspector.goToDefinition', goToDefinition);
 	vscode.workspace.onDidChangeConfiguration((e) => {
 		if (e.affectsConfiguration('bevyInspector.url')) {
 			remoteService.url = vscode.workspace.getConfiguration('bevyInspector').get('url', DEFAULT_URL);
@@ -37,4 +39,38 @@ export function activate(context: vscode.ExtensionContext) {
 			polling.restart();
 		}
 	});
+}
+
+async function goToDefinition(component: Component) {
+	if (!component?.name) {
+		vscode.window.showWarningMessage('No selected component.');
+		return;
+	}
+	const location = await findSymbolLocation(component.name);
+	if (location && location.uri) {
+		const document = await vscode.workspace.openTextDocument(location.uri);
+		const editor = await vscode.window.showTextDocument(document);
+		editor.revealRange(location.range, vscode.TextEditorRevealType.InCenter);
+		editor.selection = new vscode.Selection(location.range.start, location.range.start);
+	} else {
+		await vscode.window.showInformationMessage(`No symbol found in workspace for component "${component.name}".`);
+	}
+}
+
+async function findSymbolLocation(componentName: string): Promise<vscode.Location | null> {
+	const query = (componentName.match(/(?:\w+::)*(\w+)/) || [])[1];
+	if (query) {
+		const symbols = await vscode.commands.executeCommand<vscode.SymbolInformation[]>('vscode.executeWorkspaceSymbolProvider', query);
+		if (symbols && symbols.length > 0) {
+			console.debug(`Found symbols for query "${query}": "${symbols.map(symbol => symbol.name)}".`);
+			const firstSymbol = symbols.reduce((previous, current) => {
+				const prevDistance = levenshtein.get(query, previous.name);
+				const currDistance = levenshtein.get(query, current.name);
+				return currDistance < prevDistance ? current : previous;
+			}, symbols[0]);
+			console.debug(`Found symbol for query "${query}": "${JSON.stringify(firstSymbol)}".`);
+			return firstSymbol.location;
+		}
+	}
+	return null;
 }
