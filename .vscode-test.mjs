f3b18af522a30b4dba9ef62dbacfcf7e48d5b729
Sébastien Job <sebastien.job@gmail.com>
2025-01-30 23:25:11 +0100

    Convert project into multi-packages

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from '@vscode/test-cli';
+import { tmpdir } from 'node:os';
 
 export default defineConfig({
-	files: 'out/test/**/*.test.js',
+    // extensionDevelopmentPath: 'packages/vscode-extension',
+    files: 'packages/vscode-extension/out/test/**/*.test.js',
+    launchArgs: ['--user-data-dir', `${tmpdir()}`],
 });
---
+++ packages/vscode-extension/esbuild.mjs
@@ -0,0 +1,47 @@
+import * as esbuild from 'esbuild';
+import * as process from 'node:process';
+
+const production = process.argv.includes('--production');
+
+async function main() {
+    console.info(`Production mode: ${production ? 'enabled' : 'disabled'}`);
+    const ctx = await esbuild.context({
+        entryPoints: ['src/extension.ts'],
+        bundle: true,
+        format: 'cjs',
+        minify: production,
+        sourcemap: !production,
+        sourcesContent: false,
+        platform: 'node',
+        target: 'node20',
+        outdir: 'dist',
+        external: ['vscode'],
+        logLevel: 'silent',
+        plugins: [esbuildProblemMatcherPlugin],
+    });
+    await ctx.rebuild();
+    await ctx.dispose();
+}
+
+/** @type {import('esbuild').Plugin} */
+const esbuildProblemMatcherPlugin = {
+    name: 'esbuild-problem-matcher',
+
+    setup(build) {
+        build.onStart(() => {
+            console.log('[watch] build started');
+        });
+        build.onEnd((result) => {
+            result.errors.forEach(({ text, location }) => {
+                console.error(`✘ [ERROR] ${text}`);
+                console.error(`    ${location.file}:${location.line}:${location.column}:`);
+            });
+            console.log('[watch] build finished');
+        });
+    },
+};
+
+main().catch((e) => {
+    console.error(e);
+    process.exit(1);
+});
