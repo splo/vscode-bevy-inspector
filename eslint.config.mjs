b527e914a5520934d256956541296fc0e20b627a
Sébastien Job <sebastien.job@gmail.com>
2025-03-01 20:08:21 +0100

    Improve ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,54 @@
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
+import eslint from '@eslint/js';
+import prettier from 'eslint-plugin-prettier/recommended';
+import react from 'eslint-plugin-react';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 /** @type { import("eslint").Linter.Config[] } */
-export default [
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  tseslint.configs.stylistic,
+  prettier,
   {
-    files: ['**/*.ts'],
+    name: '@bevy-inspector/prettier',
+    rules: {
+      'prettier/prettier': 'warn',
+    },
   },
   {
-    plugins: {
-      '@typescript-eslint': typescriptEslint,
-    },
-
-    languageOptions: {
-      parser: tsParser,
-      ecmaVersion: 2022,
-      sourceType: 'module',
-    },
-
+    name: '@bevy-inspector/general',
+    ignores: ['dist'],
     rules: {
-      '@typescript-eslint/naming-convention': [
-        'warn',
-        {
-          selector: 'import',
-          format: ['camelCase', 'PascalCase'],
-        },
-      ],
-
       curly: 'warn',
       eqeqeq: 'warn',
       'no-throw-literal': 'warn',
-      semi: 'warn',
     },
   },
-];
+  {
+    name: '@bevy-inspector/vscode-extension',
+    files: ['packages/vscode-extension/**/*.ts'],
+    ignores: ['packages/vscode-extension/{out,.vscode-test}/**/*'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.es2023,
+      },
+      ecmaVersion: 2023,
+    },
+  },
+  {
+    name: '@bevy-inspector/selection-view',
+    files: ['packages/selection-view/**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+    ignores: ['packages/selection-view/out/**/*'],
+    ...react.configs.flat.recommended,
+    languageOptions: {
+      ...react.configs.flat.recommended.languageOptions,
+      globals: {
+        ...globals.browser,
+        ...globals.es2020,
+      },
+      ecmaVersion: 2020,
+    },
+  },
+);
