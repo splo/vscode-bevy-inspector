01c478db2c7037643ad44176c3e0289921e29606
Sébastien Job <sebastien.job@gmail.com>
2025-03-11 22:34:08 +0100

    Improve BRP definitions

--- packages/vscode-extension/src/brp.ts
+++ packages/vscode-extension/src/brp.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export interface BevyRemoteService {
   get(params: BevyGetParams): Promise<BevyGetResult>;
   query(params: BevyQueryParams): Promise<BevyQueryResult>;
@@ -21,7 +22,7 @@ export interface BevyError {
 export type EntityId = number;
 export type TypePath = string;
 
-export type Schema = {
+export interface Schema {
   shortPath: string;
   typePath: TypePath;
   modulePath?: string;
@@ -37,32 +38,32 @@ export type Schema = {
   oneOf?: (string | Schema)[];
   prefixItems?: Reference[];
   items?: false | Reference;
-};
+}
 
 export type SchemaKind = 'Enum' | 'List' | 'Array' | 'Map' | 'Set' | 'Struct' | 'Tuple' | 'TupleStruct' | 'Value';
 export type SchemaType = 'string' | 'float' | 'uint' | 'int' | 'object' | 'array' | 'boolean' | 'set';
-export type Reference = {
+export interface Reference {
   type: {
     $ref: string;
   };
-};
+}
 
-export type BevyGetParams = {
+export interface BevyGetParams {
   entity: EntityId;
   components: TypePath[];
   strict?: boolean;
-};
+}
 
-export type BevyGetLenientResult = {
+export interface BevyGetLenientResult {
   components: Record<TypePath, any>;
   errors: Record<TypePath, BevyError>;
-};
+}
 
 export type BevyGetStrictResult = Record<TypePath, any>;
 
 export type BevyGetResult = BevyGetLenientResult | BevyGetStrictResult;
 
-export type BevyQueryParams = {
+export interface BevyQueryParams {
   data: {
     components?: TypePath[];
     option?: TypePath[];
@@ -72,7 +73,7 @@ export type BevyQueryParams = {
     with?: TypePath[];
     without?: TypePath[];
   };
-};
+}
 
 export type BevyQueryResult = {
   entity: EntityId;
@@ -80,75 +81,75 @@ export type BevyQueryResult = {
   has?: Record<TypePath, boolean>;
 }[];
 
-export type BevySpawnParams = {
+export interface BevySpawnParams {
   components: Record<TypePath, any>;
-};
+}
 
-export type BevySpawnResult = {
+export interface BevySpawnResult {
   entity: EntityId;
-};
+}
 
-export type BevyDestroyParams = {
+export interface BevyDestroyParams {
   entity: EntityId;
-};
+}
 
 export type BevyDestroyResult = null;
 
-export type BevyRemoveParams = {
+export interface BevyRemoveParams {
   entity: EntityId;
   components: Record<TypePath, any>;
-};
+}
 
 export type BevyRemoveResult = null;
 
-export type BevyInsertParams = {
+export interface BevyInsertParams {
   entity: EntityId;
   components: Record<TypePath, any>;
-};
+}
 
 export type BevyInsertResult = null;
 
-export type BevyReparentParams = {
+export interface BevyReparentParams {
   entities: EntityId[];
   parent?: EntityId;
-};
+}
 
 export type BevyReparentResult = null;
 
-export type BevyListParams = {
+export interface BevyListParams {
   entity: EntityId;
-};
+}
 
 export type BevyListResult = string[];
 
-export type BevyRegistrySchemaParams = {
+export interface BevyRegistrySchemaParams {
   withoutCrates?: string[];
   withCrates?: string[];
   typeLimit?: {
     without?: string[];
     with?: string[];
   };
-};
+}
 
 export type BevyRegistrySchemaResult = Record<TypePath, Schema>;
 
-export type BevyGetWatchParams = {
+export interface BevyGetWatchParams {
   entity: EntityId;
   components: TypePath[];
   strict?: boolean;
-};
+}
 
-export type BevyGetWatchResult = {
+export interface BevyGetWatchResult {
   components: Record<TypePath, any>;
   removed: TypePath[];
   errors?: Record<TypePath, BevyError>;
-};
+}
 
-export type BevyListWatchParams = {
+export interface BevyListWatchParams {
   entity: EntityId;
-};
+}
 
-export type BevyListWatchResult = {
+export interface BevyListWatchResult {
   added: TypePath[];
   removed: TypePath[];
-};
+}
