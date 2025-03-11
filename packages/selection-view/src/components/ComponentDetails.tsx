942706bd0c003ca24d0f66da74310a2cdaa006b1
Sébastien Job <sebastien.job@gmail.com>
2025-03-11 23:02:38 +0100

    feat: Allow to edit components value

--- packages/selection-view/src/components/ComponentDetails.tsx
+++ packages/selection-view/src/components/ComponentDetails.tsx
@@ -3,20 +3,25 @@ import {
   GetSchemaRequestData,
   GetSchemaResponseData,
   InspectorMessage,
+  SetComponentValueRequestData,
+  SetComponentValueResponseData,
 } from '@bevy-inspector/inspector-messages';
 import '@vscode-elements/elements/dist/vscode-collapsible';
 import '@vscode-elements/elements/dist/vscode-form-group';
 import '@vscode-elements/elements/dist/vscode-label';
 import '@vscode-elements/elements/dist/vscode-textfield';
-import { useMemo } from 'react';
+import { VscodeTextfield } from '@vscode-elements/elements/dist/vscode-textfield';
+import { FormEvent, useMemo } from 'react';
 import { useRequest } from '../useRequest';
+import { messenger } from '../vscodeMessenger';
 import { ErrorCard } from './ErrorCard';
 
 interface ComponentProps {
+  entityId: number;
   component: Component;
 }
 
-export function ComponentDetails({ component }: ComponentProps) {
+export function ComponentDetails({ entityId, component }: ComponentProps) {
   const data: GetSchemaRequestData = useMemo(
     () => ({
       componentTypePath: component.typePath,
@@ -34,9 +39,11 @@ export function ComponentDetails({ component }: ComponentProps) {
       open={!component.error}
     >
       {typeof component.value === 'undefined' ? null : (
-        <vscode-form-group variant="horizontal">
-          <vscode-textfield value={JSON.stringify(component.value)}></vscode-textfield>
-        </vscode-form-group>
+        <>
+          <vscode-form-group variant="horizontal">
+            <vscode-textfield onInput={onValueChanged} value={JSON.stringify(component.value)}></vscode-textfield>
+          </vscode-form-group>
+        </>
       )}
       {component.error ? (
         <>
@@ -46,4 +53,13 @@ export function ComponentDetails({ component }: ComponentProps) {
       ) : null}
     </vscode-collapsible>
   );
+
+  function onValueChanged(event: FormEvent<VscodeTextfield>) {
+    const data: SetComponentValueRequestData = {
+      entityId,
+      typePath: component.typePath,
+      newValue: JSON.parse(event.currentTarget.value),
+    };
+    messenger.sendRequest<SetComponentValueResponseData>(InspectorMessage.SetComponentValue, data).then(console.log);
+  }
 }
