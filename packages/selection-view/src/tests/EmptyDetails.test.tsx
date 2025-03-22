0736c0547986007df09843167b4435903bef33d2
Sébastien Job <sebastien.job@gmail.com>
2025-03-22 17:59:53 +0100

    Remove useless vitest tests

--- packages/selection-view/src/tests/EmptyDetails.test.tsx
+++
@@ -1,9 +0,0 @@
-/// <reference types="@vitest/browser/providers/playwright" />
-import { expect, test } from 'vitest';
-import { render } from 'vitest-browser-react';
-import { EmptyDetails } from '../components/EmptyDetails';
-
-test('renders empty', async () => {
-  const { getByText } = render(<EmptyDetails />);
-  await expect.element(getByText('Nothing selected in the tree.')).toBeInTheDocument();
-});
