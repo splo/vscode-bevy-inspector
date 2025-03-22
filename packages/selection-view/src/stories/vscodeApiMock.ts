e81211b679af99ca73bbb9296644369a46d5126e
Sébastien Job <sebastien.job@gmail.com>
2025-03-22 18:03:12 +0100

    Add VsCodeApi mock to Storybook

---
+++ packages/selection-view/src/stories/vscodeApiMock.ts
@@ -0,0 +1,31 @@
+import type { WebviewApi } from 'vscode-webview';
+
+declare global {
+  interface Window {
+    vscodeApiMock: VsCodeApiMock;
+  }
+}
+
+export class VsCodeApiMock implements WebviewApi<unknown> {
+  public handler: (message: unknown) => void = console.log;
+
+  constructor() {
+    // Bind the postMessage method to the current instance.
+    this.postMessage = this.postMessage.bind(this);
+  }
+
+  public postMessage(message: unknown) {
+    this.handler(message);
+  }
+
+  public getState() {
+    const state = sessionStorage.getItem('vscodeState');
+    return state ? JSON.parse(state) : undefined;
+  }
+
+  public setState<T>(newState: T): T {
+    sessionStorage.setItem('vscodeState', JSON.stringify(newState));
+    return newState;
+  }
+}
+    
