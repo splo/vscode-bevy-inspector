91d3fdf56566926898ba316ae5037f0fd1449208
Sébastien Job <sebastien.job@gmail.com>
2025-04-10 15:48:03 +0200

    Fix watch mode

--- packages/vscode-extension/esbuild.mjs
+++ packages/vscode-extension/esbuild.mjs
@@ -3,9 +3,11 @@ import * as console from 'node:console';
 import * as process from 'node:process';
 
 const production = process.argv.includes('--production');
+const watch = process.argv.includes('--watch');
 
 async function main() {
   console.info(`Production mode: ${production ? 'enabled' : 'disabled'}`);
+  console.info(`Watch mode: ${watch ? 'enabled' : 'disabled'}`);
   const ctx = await esbuild.context({
     entryPoints: ['src/index.ts'],
     bundle: true,
@@ -13,6 +15,7 @@ async function main() {
     minify: production,
     sourcemap: !production,
     sourcesContent: false,
+    legalComments: 'none',
     platform: 'node',
     target: 'node20',
     outdir: '../../dist/vscode-extension',
@@ -20,8 +23,13 @@ async function main() {
     logLevel: 'silent',
     plugins: [esbuildProblemMatcherPlugin],
   });
-  await ctx.rebuild();
-  await ctx.dispose();
+  if (watch) {
+    await ctx.watch();
+    return;
+  } else {
+    await ctx.rebuild();
+    await ctx.dispose();
+  }
 }
 
 /** @type {import('esbuild').Plugin} */
@@ -30,14 +38,14 @@ const esbuildProblemMatcherPlugin = {
 
   setup(build) {
     build.onStart(() => {
-      console.log('[watch] build started');
+      console.log(`${watch ? '[watch] ' : ''}build started`);
     });
     build.onEnd((result) => {
       result.errors.forEach(({ text, location }) => {
         console.error(`✘ [ERROR] ${text}`);
         console.error(`    ${location.file}:${location.line}:${location.column}:`);
       });
-      console.log('[watch] build finished');
+      console.log(`${watch ? '[watch] ' : ''}build finished`);
     });
   },
 };
